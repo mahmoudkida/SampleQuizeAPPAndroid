8a228a7866acbaf8d211ec17b77d6df8730ca6ff
mahmoudkida <mahmoudkida@gmail.com>
2018-04-26 11:12:49 +0300

    feat: addqesutions and anwsers to resources make the submission button work show the toast with with the result

--- app/src/main/java/com/wshwsh/sample_quize_app_android/Videogame_Quiz.java
+++ app/src/main/java/com/wshwsh/sample_quize_app_android/Videogame_Quiz.java
@@ -1,16 +1,17 @@
 package com.wshwsh.sample_quize_app_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class Videogame_Quiz extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +20,6 @@ public class Videogame_Quiz extends AppCompatActivity {
 
     private int totalMark = 0;
     private int numberOfQuestions = 4;
-
     String Anwser1 = "Sony";
     String Anwser2 = "Zelda";
     String Anwser3A = "Pikatchu";
@@ -27,51 +27,51 @@ public class Videogame_Quiz extends AppCompatActivity {
     String Anwser3C = "Charmender";
     String Anwser4 = "Blue";
 
-    public void viewAnwsers(View view) {
-
-        if(checkForAnwser("radiobutton", Anwser1,new String[]{Anwser1})){
-            totalMark ++;
+    public void viewAnswers(View view) {
+        totalMark = 0;
+        if (checkForAnwser("radiobutton", Anwser1, new String[]{Anwser1})) {
+            totalMark++;
         }
-        if(checkForAnwser("textbox", Anwser2,new String[]{Anwser2})){
-            totalMark ++;
+        if (checkForAnwser("textbox", "question2anwser", new String[]{Anwser2})) {
+            totalMark++;
         }
-
-        if(checkForAnwser("checbox", Anwser4,new String[]{Anwser3A,Anwser3B,Anwser3C})){
-            totalMark ++;
+        if (checkForAnwser("checbox", Anwser4, new String[]{Anwser3A, Anwser3B, Anwser3C})) {
+            totalMark++;
         }
-
-        if(checkForAnwser("radiobutton", Anwser4,new String[]{Anwser4})){
-            totalMark ++;
+        if (checkForAnwser("radiobutton", Anwser4, new String[]{Anwser4})) {
+            totalMark++;
         }
+        Context context = getApplicationContext();
+        CharSequence text = "Your test score is " + totalMark + " of " + numberOfQuestions;
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     private boolean checkForAnwser(String anwserType, String inputName, String[] anwser) {
-        if (anwserType == "textbox") {
+        if (anwserType.equals("textbox")) {
             EditText anwserInput = (EditText) findViewById(getResources().getIdentifier(inputName, "id", getPackageName()));
-            if(anwser[0] == anwserInput.getText().toString()){
+            if (anwser[0].equalsIgnoreCase(anwserInput.getText().toString())) {
                 return true;
             }
-        } else if (anwserType == "radiobutton") {
+        } else if (anwserType.equals("radiobutton")) {
             RadioButton anwserInput = (RadioButton) findViewById(getResources().getIdentifier(inputName, "id", getPackageName()));
-            if(anwserInput.isChecked()){
+            if (anwserInput.isChecked()) {
                 return true;
             }
-        } else if (anwserType == "checbox") {
+        } else if (anwserType.equals("checbox")) {
             int anwserLength = anwser.length;
             int correctAnwsers = 0;
-            for (int anwserIndex = 0 ; anwserIndex < anwserLength ;anwserIndex++){
+            for (int anwserIndex = 0; anwserIndex < anwserLength; anwserIndex++) {
                 CheckBox anwserInput = (CheckBox) findViewById(getResources().getIdentifier(anwser[anwserIndex], "id", getPackageName()));
-                if(anwserInput.isChecked()){
-                    correctAnwsers ++;
+                if (anwserInput.isChecked()) {
+                    correctAnwsers++;
                 }
             }
-            if(correctAnwsers == anwserLength){
+            if (correctAnwsers == anwserLength) {
                 return true;
             }
-
         }
-
-
         return false;
     }
 }
